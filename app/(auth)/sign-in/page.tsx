b23b392809c9719d445687a2c7dadf74d2bdd34d
Bupalchow <bupalchowdary06@gmail.com>
2025-02-13 20:20:09 +0530

    mobilenav

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/ui/Forms/AuthForm";
 import React from "react";
 
+import { SignInSchema } from "@/lib/validation";
 const signIn = () => {
-  return <div>Sign In</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default signIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/ui/Forms/AuthForm";
+import { SignUpSchema } from "@/lib/validation";
 import React from "react";
 
 const signUp = () => {
-  return <div>Sign Up</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default signUp;
